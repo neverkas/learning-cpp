809b2f35ef80ca2c5ae4713aacdbadff53bab101
neverkas <emmanuel.lazarte.011@gmail.com>
2018-04-22 19:04:04 -0300

    It was added a new exercise and change some things in the previous one

---
+++ Exercise-2.cpp
@@ -0,0 +1,43 @@
+/*
+ * Exercise 2:
+ * Dados dos valores numericos enteros a y b 
+ * calcular e informar el cosiente a/b
+ * Considere que b puede ser cero,
+ * en ese caso mostrar el correspondiente mensaje de error
+*/
+
+#include <iostream>
+
+using namespace std;
+
+// declare function dividir
+int dividir(int a, int b);
+
+int main(){
+  int a, b, result;
+
+  cout << "Ingrese el primer numero ";
+  cin >> a;
+
+  cout << "Ingrese el segundo numero ";
+  cin >> b;
+
+  try{
+    result = dividir(a, b);
+    cout << "La divisón a/b es " << result << endl;
+    
+  } catch(const char* msg){
+    cerr << msg << endl;
+  }
+
+  return 0;
+}
+
+// function dividir
+int dividir(int a, int b){
+  if(b == 0){
+    throw "No se puede dividir por cero!";
+  }
+  
+  return (a / b);
+}
